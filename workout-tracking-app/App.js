e0e70d51ba2d3f2f3729eefe405860b17c6b1d9f
Abrar Zaman <abrarzaman2003@gmail.com>
2022-12-20 14:58:14 -0600

    added firebase and firestore set up code

--- workout-tracking-app/App.js
+++ workout-tracking-app/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { db } from './backend/firebaseSetup';
 
 export default function App() {
+  const test = async () => {
+    console.log("test")
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='backend testing lol' onPress={test}/>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
---
+++ workout-tracking-app/backend/firebaseSetup.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAm4Xbm6f1c-zTiNZ-g6kSEN4VOg0Mdsvg",
+  authDomain: "workout-tracking-app-1fcb5.firebaseapp.com",
+  projectId: "workout-tracking-app-1fcb5",
+  storageBucket: "workout-tracking-app-1fcb5.appspot.com",
+  messagingSenderId: "257614975370",
+  appId: "1:257614975370:web:45928cb3781d37c47fb873",
+  measurementId: "G-VQE5LMTHQ7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+export const db = getFirestore(app); //exporting db so we can use in other firestore functions
